95970c82dac64e543ee7c15c5d9147fe747d6494
nuxie <zkalinowska96@icloud.com>
2020-02-06 09:19:10 +0100

    Minor fixes

---
+++ main.py
@@ -0,0 +1,60 @@
+from des import encrypt, decrypt
+import unit_tests
+
+
+def main_menu():
+    print("Prosze wybrac opcje:")
+    print("1 - Zaszyfrowanie tekstu")
+    print("2 - Odszyfrowanie tekstu")
+    print("3 - Testy jednostkowe i wyjscie")
+    print("0 - Wyjscie")
+    choice = input(" >>  ")
+    exec_menu(choice)
+    return
+
+
+def exec_menu(choice):
+
+    if choice == "1":
+        key = []
+        print("Prosze wprowadzic nazwe pliku z kluczem - kazdy bajt klucza podaj w osobnej linii...")
+        key_file_name = input(" >> ")
+        print("Prosze wprowadzic nazwe pliku z tekstem...")
+        msg_file_name = input(" >> ")
+        with open(key_file_name, 'r') as key_file:
+            key = [int(line.strip()) for line in key_file]
+        with open(msg_file_name, 'r') as msg_file:
+            msg = msg_file.read().replace('\n', ' ')
+        print("Zaszyfrowany tekst w postaci bajtow zapisano w pliku encrypted.txt.")
+        encrypted = encrypt(key, msg)
+        with open('encrypted.txt', 'w') as output_file:
+            for byte in encrypted:
+                output_file.write("%s\n" % byte)
+        main_menu()
+
+    if choice == "2":
+        key = []
+        print("Prosze wprowadzic nazwe pliku z kluczem - kazdy bajt klucza podaj w osobnej linii...")
+        key_file_name = input(" >> ")
+        print("Prosze wprowadzic nazwe pliku z zaszyfrowanymi bajtami...")
+        msg_file_name = input(" >> ")
+        with open(key_file_name, 'r') as key_file:
+            key = [int(line.strip()) for line in key_file]
+        with open(msg_file_name, 'r') as msg_file:
+            msg = [int(line.strip()) for line in msg_file]
+        print("Odszyfrowany tekst zapisano w pliku decrypted.txt.")
+        encrypted = decrypt(key, msg)
+        with open('decrypted.txt', 'w') as output_file:
+            for byte in encrypted:
+                output_file.write("%s" % byte)
+        main_menu()
+
+    if choice == "3":
+        unit_tests.unittest.main()
+
+    if choice == "0":
+        return
+
+
+if __name__ == "__main__":
+    main_menu()
--- unit_tests.py
+++ unit_tests.py
@@ -1,6 +1,6 @@
 import unittest
 import random
-from des import encryption, decryption
+from des import encrypt, decrypt
 import helper
 
 class TestDES(unittest.TestCase):
@@ -9,8 +9,8 @@ class TestDES(unittest.TestCase):
         key = [0, 252, 132, 99, 12, 3, 17, 5]
         plaintext = "Python is an interpreted, high-level, general-purpose programming language. " \
                     "It was created by Guido van Rossum and first released in 1991."
-        ciphertext = encryption(key, plaintext)
-        self.assertTrue(decryption(key, ciphertext) == plaintext)
+        ciphertext = encrypt(key, plaintext)
+        self.assertTrue(decrypt(key, ciphertext) == plaintext)
 
     def testBasicEN_RANDKEY(self):
         key = []
@@ -18,7 +18,7 @@ class TestDES(unittest.TestCase):
             key.append(random.randint(0, 255))
         plaintext = "Python is an interpreted, high-level, general-purpose programming language. " \
                     "It was created by Guido van Rossum and first released in 1991."
-        ciphertext = encryption(key, plaintext)
+        ciphertext = encrypt(key, plaintext)
         self.assertTrue(decryption2(key, ciphertext) == plaintext)
 
     def testOnlyNumbers(self):
@@ -28,23 +28,23 @@ class TestDES(unittest.TestCase):
         plaintext = "1 231 3123 111"
         ciphertext = encryption(key, plaintext)
         print(helper.byte_to_str(ciphertext))
-        self.assertTrue(decryption(key, ciphertext) == plaintext)
+        self.assertTrue(decrypt(key, ciphertext) == plaintext)
 
     def testShortKey(self):
         key = [0, 252, 132, 99, 12]
-        self.assertRaises(AssertionError, lambda: encryption(key, "ala"))
+        self.assertRaises(AssertionError, lambda: encrypt(key, "ala"))
 
     def testLongKey(self):
         key = [0, 252, 132, 99, 12, 3, 17, 5, 156]
-        self.assertRaises(AssertionError, lambda: encryption(key, "ma"))
+        self.assertRaises(AssertionError, lambda: encrypt(key, "ma"))
 
     def testWrongKeyValPos(self):
         key = [0, 252, 132, 99, 12, 3, 17, 999]
-        self.assertRaises(AssertionError, lambda: encryption(key, "kota"))
+        self.assertRaises(AssertionError, lambda: encrypt(key, "kota"))
 
     def testWrongKeyValNeg(self):
         key = [0, 252, 132, 99, 12, 3, 17, -3]
-        self.assertRaises(AssertionError, lambda: encryption(key, "kota"))
+        self.assertRaises(AssertionError, lambda: encrypt(key, "kota"))
 
 if __name__ == "__main__":
     unittest.main()
