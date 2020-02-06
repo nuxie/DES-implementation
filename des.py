6e800039e2c2c70d97eac5958963fd826482bf33
nuxie <zkalinowska96@icloud.com>
2020-02-06 09:15:09 +0100

    Refactoring and minor bug fixes

--- des.py
+++ des.py
@@ -2,45 +2,52 @@ import random
 import constants
 import helper
 
-def encryption(key, plain_text):
-    """ Encrypt text with a given key.
+def manipulate(key, byte_input, option):
+    """ Check the key correctness , generate subkeys and perform a desired block transformation.
 
-    :param key: key 
+    :param key: key - default to 0 meaning that a random one will be generated
     :type: list
-    :param plain_text: message to be encrypted
-    :type: str
+    :param byte_input: message to be manipulated in the byte form
+    :type: list
+    :param byte_input: "ENCRYPT" or "DECRYPT"
+    :type: str  
     
-    :return: encrypted message, key
-    :rtype: str, list
+    :return: result of transformation in byte form
+    :rtype: list
     """
     byte_output = []
-    byte_input = helper.str_to_byte(plain_text)
     assert (len(key) == 8), "Klucz jest zlej dlugosci!"
     assert (all(i <= 255 for i in key)), "Klucz ma za duza wartosc!"
     assert (all(i >= 0 for i in key)), "Klucz ma za mala wartosc!"
     subkeys = helper.set_subkeys(key)
     for i in range(0, len(byte_input), 8):
-        byte_output += helper.transform_block(byte_input[i:i+8], subkeys, "ENCRYPT")
+        byte_output += helper.transform_block(
+            byte_input[i:i+8], subkeys, option)
     return byte_output
 
+def encrypt(key, plain_text):
+    """ Encrypt text with a given key.
 
-def decryption(key, cipher_text):
+    :param key: key - default to 0 meaning that a random one will be generated
+    :type: list
+    :param plain_text: message to be encrypted
+    :type: str
+    
+    :return: encrypted message
+    :rtype: str
+    """
+    return manipulate(key, helper.str_to_byte(plain_text), "ENCRYPT")
+
+
+def decrypt(key, byte_input):
     """ Decrypt text with a given key.
 
     :param key: key
     :type: list
-    :param cipher_text: message to be decrypted (byte form)
-    :type: str
+    :param byte_input: message to be decrypted (byte form)
+    :type: list
 
     :return: decrypted message
     :rtype: str
     """
-    byte_output = []
-    assert (len(key) == 8), "Klucz jest zlej dlugosci!"
-    assert (all(i <= 255 for i in key)), "Klucz ma za duza wartosc!"
-    assert (all(i >= 0 for i in key)), "Klucz ma za mala wartosc!"
-    subkeys = helper.set_subkeys(key)
-    for i in range(0,len(cipher_text), 8):
-        byte_output += helper.transform_block(
-            cipher_text[i:i+8], subkeys, "DECRYPT")
-    return helper.byte_to_str(byte_output)
+    return helper.byte_to_str(manipulate(key, byte_input, "DECRYPT"))
